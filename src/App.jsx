15725ad2202e6f345a9510924908fc9eb80e8831
hanif-alkahfy <hanifalkahfy@gmail.com>
2025-03-14 10:50:49 +0700

    add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Features from "./components/Features";
 import ProductDetail from "./components/ProductDetail";
 import ContactUs from "./components/ContactUs";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
     <div className="flex flex-col w-screen min-h-screen bg-[#F0F9FF] text-[#01579B]">
+      <Analytics/>
       <Hero />
       <Features />
       <ProductDetail />
